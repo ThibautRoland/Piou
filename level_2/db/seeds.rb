8da03b82da124e0a445fff80a198e381a5144132
thibaut roland <thibaut.roland2@gmail.com>
2024-06-19 18:40:22 +0200

    seed tags

--- level_2/db/seeds.rb
+++ level_2/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+TAGS = %w[urgent secret rigolo].freeze
+
+puts 'destroying all tags...'
+Tag.destroy_all
+
+TAGS.each do |tag|
+  Tag.create(tag:)
+end
+
+puts 'tags created!'
