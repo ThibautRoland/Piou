6319adf1172fae873a0cca95fb15a1d9045d5b80
thibaut roland <thibaut.roland2@gmail.com>
2024-06-19 18:30:22 +0200

    add validations for message and tag models, add one-to-many relationships between the models

--- level_2/app/models/tag.rb
+++ level_2/app/models/tag.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Tag < ApplicationRecord
+  has_many :messages
+
+  # VALID_TAGS = ['urgent', 'secret', 'rigolo']
+  VALID_TAGS = %w[urgent secret rigolo].freeze
+
+  validates :tag, presence: true, inclusion: { in: VALID_TAGS, message: 'This is not a valid tag' }
 end
--- level_2/app/models/user.rb
+++ level_2/app/models/user.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, :first_name, :last_name, presence: true
+
+  has_many :messages
 end
