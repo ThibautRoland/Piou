118dc287b24c93732b9f4f9954408467baabc865
thibaut roland <thibaut.roland2@gmail.com>
2024-06-19 18:55:08 +0200

    seed 10 users and 20 messages

--- level_2/db/seeds.rb
+++ level_2/db/seeds.rb
@@ -5,14 +5,50 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
 
 TAGS = %w[urgent secret rigolo].freeze
 
 puts 'destroying all tags...'
 Tag.destroy_all
+puts 'destroying all messages...'
+Message.destroy_all
+puts 'destroying all users...'
+User.destroy_all
 
 TAGS.each do |tag|
-  Tag.create(tag:)
+  Tag.create!(tag:)
 end
 
 puts 'tags created!'
+
+User.create!(
+  email: 'goku@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  first_name: 'Son',
+  last_name: 'Goku'
+)
+
+9.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+
+puts 'users created!'
+
+# Create Messages
+20.times do
+  Message.create!(
+    content: Faker::Lorem.sentence,
+    user: User.order('RANDOM()').first,
+    tag: Tag.order('RANDOM()').first
+  )
+end
+
+puts 'messages created!'
