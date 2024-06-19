c6275d260b994dd177bcf622a55f5ddeeb239444
thibaut roland <thibaut.roland2@gmail.com>
2024-06-19 19:27:30 +0200

    add show method in the messages controller and show view, in the messages_controller include the translation module to translate the message in the piou language

--- level_2/app/controllers/messages_controller.rb
+++ level_2/app/controllers/messages_controller.rb
@@ -1,6 +1,22 @@
+require_relative '../../../level_1/translation_service'
+
 class MessagesController < ApplicationController
+
+  include Translation
+
+  before_action :set_message, only: :show
 
   def index
     @messages = Message.all
+  end
+
+  def show
+    @translation = translate('to_piou', @message.content)
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
