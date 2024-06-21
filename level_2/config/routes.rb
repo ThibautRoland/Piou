fcce5e5ecc047b98ce476da2b65b3ddca8c29773
thibaut roland <thibaut.roland2@gmail.com>
2024-06-21 15:14:44 +0200

    add small landing page with links to sign in, sign up and index messages

--- level_2/config/routes.rb
+++ level_2/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
   resources :messages, only: %i[index show create]
 
   get '/tags/:name/messages', to: 'messages#messages_by_tag', as: :tag_messages
